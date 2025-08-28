2c582e5cdb20a721e8e6d5af55cd0c365db191a7
Simone Masoero <smsimone65@gmail.com>
2025-08-28 10:51:27 +0200

    modificata vista progetto

--- src/api/controller/events.ts
+++ src/api/controller/events.ts
@@ -4,10 +4,13 @@ import db from '../../db';
 
 export async function getProjectEvents(req: Request, res: Response) {
     const projectId = parseInt(req.params.id);
+    const { page, pageSize } = req.query;
 
     const events = await db.event.findMany({
         where: { projectId },
         orderBy: { createdAt: 'desc' },
+        skip: page ? (Number(page) * Number(pageSize)) : 0,
+        take: pageSize ? Number(pageSize) : 10
     })
 
     if (!events || events.length === 0) {
